2ae34153a2bfefceb1efe7c28fcc56d43f6f025c
na57 <na57@qq.com>
2016-07-30 13:20:31 +0800

    publish 0.1.0

--- dist/index.js
+++ dist/index.js
@@ -11,36 +11,53 @@ var mongodb = require('mongodb');
 var _this = this;
 
 var useCollection = function () {
-  var _ref = _asyncToGenerator(_regeneratorRuntime.mark(function _callee(url, collectionName, cb) {
-    var db, col, result;
+  var _ref = _asyncToGenerator(_regeneratorRuntime.mark(function _callee(url, collectionName, success) {
+    var fail = arguments.length <= 3 || arguments[3] === undefined ? function () {
+      return console.log('userCollection: FAILED');
+    } : arguments[3];
+    var result, db, col;
     return _regeneratorRuntime.wrap(function _callee$(_context) {
       while (1) {
         switch (_context.prev = _context.next) {
           case 0:
-            _context.next = 2;
+            // eslint-disable-line no-console
+            result = null;
+            _context.prev = 1;
+            _context.next = 4;
             return mongodb.MongoClient.connect(url);
 
-          case 2:
+          case 4:
             db = _context.sent;
             col = db.collection(collectionName);
-            _context.next = 6;
-            return cb(col, db);
+            _context.next = 8;
+            return success(col, db);
 
-          case 6:
+          case 8:
             result = _context.sent;
 
             db.close();
+            _context.next = 15;
+            break;
+
+          case 12:
+            _context.prev = 12;
+            _context.t0 = _context['catch'](1);
+
+            fail(_context.t0);
+
+          case 15:
+            _context.prev = 15;
             return _context.abrupt('return', result);
 
-          case 9:
+          case 18:
           case 'end':
             return _context.stop();
         }
       }
-    }, _callee, _this);
+    }, _callee, _this, [[1, 12, 15, 18]]);
   }));
 
-  return function useCollection(_x, _x2, _x3) {
+  return function useCollection(_x, _x2, _x3, _x4) {
     return _ref.apply(this, arguments);
   };
 }();
--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
-// import 'babel-polyfill';
-import {MongoClient} from 'mongodb';
+import { MongoClient } from 'mongodb';
 
-export const useCollection = async (url, collectionName, cb) => {
-  let db = await MongoClient.connect(url);
-  let col = db.collection(collectionName);
-  let result = await cb(col, db);
-  db.close();
-  return result;
-}
+export const useCollection = async (url, collectionName, success,
+  fail = () => console.log('userCollection: FAILED')) => { // eslint-disable-line no-console
+  let result = null;
+  try {
+    const db = await MongoClient.connect(url);
+    const col = db.collection(collectionName);
+    result = await success(col, db);
+    db.close();
+  } catch (e) {
+    fail(e);
+  } finally {
+    return result;
+  }
+};
