fa3c981fa4b898375b370655c8db2410e6088628
na57 <na57@qq.com>
2017-06-15 12:31:12 +0800

    支持直接使用db对象

--- src/index.js
+++ src/index.js
@@ -1,24 +1,31 @@
-import { MongoClient } from 'mongodb';
+import { MongoClient, Db } from 'mongodb';
 import debug from 'debug';
 
 const error = debug('mongo-use-collection:error');
 const info = debug('mongo-use-collection:info');
 export const useCollection = async (
-  url,
+  urlOrDb,
   collectionName,
   success,
   fail = () => {}
 ) => {
   let result = null;
   try {
-    const db = await MongoClient.connect(url);
-    info('connected to ', url);
+    let db;
+    let needCloseDb = false;
+    if (urlOrDb instanceof Db) {
+      db = urlOrDb;
+    } else {
+      db = await MongoClient.connect(urlOrDb);
+      needCloseDb = true;
+      info('connected to ', urlOrDb);
+    }
     db.on('error', e => {
-      error(e);
+      error(e.message);
     });
     const col = db.collection(collectionName);
     result = await success(col, db);
-    db.close();
+    if (needCloseDb) db.close();
   } catch (e) {
     error(e.message);
     error(e.stack);
